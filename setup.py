152ff9800497eacbf7a11b3e826869b3b3098691
宮地宏樹 <hdekm852@gmail.com>
2020-08-17 09:55:07 +0900

    Update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='autoload_module',
     description='Python Autoload Module',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT License",
-    version='0.1.0',
+    version='0.1.2',
     url='https://github.com/hiroki0525/autoload_module',
     author='hiroki0525',
     author_email='nukoprogramming@gmail.com',
-    package_dir={"": "lib"},
+    packages=find_packages(exclude=['test']),
 )
