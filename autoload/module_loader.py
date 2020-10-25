a01c8568d62031dbb250284f9bee67548c596d01
宮地宏樹 <hdekm852@gmail.com>
2020-10-26 08:04:15 +0900

    Add load functions

--- autoload/module_loader.py
+++ autoload/module_loader.py
@@ -25,44 +25,13 @@ class ModuleLoader:
         return self.__load_resource(file_name, 'function')
 
     def load_class(self, file_name):
-        return self.__load_resource(file_name, 'class')
+        return self.__load_resource(file_name)
 
-    def load_classes(self, pkg_name=None, excludes=None):
-        target_dir = self.__path_fix(pkg_name)
-        if not OP.isdir(target_dir):
-            raise NotADirectoryError('Not Found The Directory : {}'.format(target_dir))
-        if target_dir not in SP:
-            SP.append(target_dir)
-        files = [OP.splitext(file)[0] for file in os.listdir(target_dir) if file.endswith('.py')]
-        exclude_files = list(DEFAULT_EXCLUDES)
-        exclude_files.append(OP.basename(self.__detect_call_path()))
-        if excludes:
-            if not iter(excludes):
-                raise TypeError('excludes variable must be iterable.')
-            for exclude in excludes:
-                if not isinstance(exclude, str):
-                    raise TypeError('The contents of the excludes must all be strings')
-                exclude_files.append(exclude)
-        fix_excludes = [exclude.replace('.py', '') for exclude in exclude_files]
-        excluded_files = tuple(set(files) - set(fix_excludes))
-        classes = []
-        for file in excluded_files:
-            module = importlib.import_module(file)
-            for mod_name, clazz in inspect.getmembers(module, inspect.isclass):
-                if hasattr(clazz, DECORATOR_ATTR) and clazz.load_flg:
-                    classes.append(clazz)
-                    break
-                if "".join(file.split("_")).lower() != mod_name.lower():
-                    continue
-                classes.append(clazz)
-        has_order_classes = [clazz for clazz in classes if hasattr(clazz, 'load_order') and clazz.load_order]
-        if not has_order_classes:
-            return tuple(classes)
-        no_has_order_classes = [clazz for clazz in classes if not hasattr(clazz, 'load_order') or not clazz.load_order]
-        if not no_has_order_classes:
-            return tuple(sorted(has_order_classes, key=lambda clazz: clazz.load_order))
-        ordered_classes = sorted(has_order_classes, key=lambda clazz: clazz.load_order) + no_has_order_classes
-        return tuple(ordered_classes)
+    def load_classes(self, pkg_name, excludes=None):
+        return self.__load_resources(pkg_name, excludes=excludes)
+
+    def load_functions(self, pkg_name, excludes=None):
+        return self.__load_resources(pkg_name, excludes=excludes, type='function')
 
     def __detect_call_path(self):
         for path in inspect.stack():
@@ -124,7 +93,7 @@ class ModuleLoader:
         path = '/'.join(name.split('.'))
         return self.__base_path + '/' + path + '/'
 
-    def __load_resource(self, file_name, type):
+    def __load_resource(self, file_name, type='class'):
         target_file = file_name.replace('.py', '') if file_name.endswith('.py') else file_name
         fix_path_arr = self.__path_fix(target_file).split('/')
         target_file = fix_path_arr[-2]
@@ -140,3 +109,42 @@ class ModuleLoader:
             if "".join(target_file.split("_")).lower() != mod_name.lower():
                 continue
             return resource
+
+    def __load_resources(self, pkg_name, excludes=None, type='class'):
+        target_dir = self.__path_fix(pkg_name)
+        if not OP.isdir(target_dir):
+            raise NotADirectoryError('Not Found The Directory : {}'.format(target_dir))
+        if target_dir not in SP:
+            SP.append(target_dir)
+        files = [OP.splitext(file)[0] for file in os.listdir(target_dir) if file.endswith('.py')]
+        exclude_files = list(DEFAULT_EXCLUDES)
+        exclude_files.append(OP.basename(self.__detect_call_path()))
+        if excludes:
+            if not iter(excludes):
+                raise TypeError('excludes variable must be iterable.')
+            for exclude in excludes:
+                if not isinstance(exclude, str):
+                    raise TypeError('The contents of the excludes must all be strings')
+                exclude_files.append(exclude)
+        fix_excludes = [exclude.replace('.py', '') for exclude in exclude_files]
+        excluded_files = tuple(set(files) - set(fix_excludes))
+        classes = []
+        # Don't use enum because it is not supported under Python 3.4 version
+        predicate = inspect.isclass if type == 'class' else inspect.isfunction
+        for file in excluded_files:
+            module = importlib.import_module(file)
+            for mod_name, clazz in inspect.getmembers(module, predicate):
+                if hasattr(clazz, DECORATOR_ATTR) and clazz.load_flg:
+                    classes.append(clazz)
+                    break
+                if "".join(file.split("_")).lower() != mod_name.lower():
+                    continue
+                classes.append(clazz)
+        has_order_classes = [clazz for clazz in classes if hasattr(clazz, 'load_order') and clazz.load_order]
+        if not has_order_classes:
+            return tuple(classes)
+        no_has_order_classes = [clazz for clazz in classes if not hasattr(clazz, 'load_order') or not clazz.load_order]
+        if not no_has_order_classes:
+            return tuple(sorted(has_order_classes, key=lambda clazz: clazz.load_order))
+        ordered_classes = sorted(has_order_classes, key=lambda clazz: clazz.load_order) + no_has_order_classes
+        return tuple(ordered_classes)
