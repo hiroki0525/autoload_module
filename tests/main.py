40ad0d107997d04d2a082d9d28fdfa4b86948ed4
宮地宏樹 <hdekm852@gmail.com>
2020-11-03 15:06:16 +0900

    Add test cases

--- tests/main.py
+++ tests/main.py
@@ -4,11 +4,15 @@ You should run this in Docker container.
 """
 
 from autoload.module_loader import ModuleLoader
-from autoload.decorator import load_config
 
 def main():
-    pass
-
+    print("start test")
+    loader = ModuleLoader()
+    print(loader.load_class(".clazz.base.module_1"))
+    print(loader.load_classes("clazz.packageA"))
+    print(loader.load_function("./func/base/func1.py"))
+    print(loader.load_function("/func/packageA"))
+    print("end test")
 
 if __name__ == "__main__":
     main()
