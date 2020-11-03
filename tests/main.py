03dca52f54dc5b4e52394e4d33619e3ab90cc489
宮地宏樹 <hdekm852@gmail.com>
2020-11-03 15:24:24 +0900

    Minor change

--- tests/main.py
+++ tests/main.py
@@ -6,13 +6,13 @@ You should run this in Docker container.
 from autoload.module_loader import ModuleLoader
 
 def main():
-    print("start test")
+    print("--- start test -------------------------")
     loader = ModuleLoader()
     print(loader.load_class(".clazz.base.module_1"))
     print(loader.load_classes("clazz.packageA"))
     print(loader.load_function("./func/base/func1.py"))
     print(loader.load_function("/func/packageA"))
-    print("end test")
+    print("--- end test -------------------------")
 
 if __name__ == "__main__":
     main()
