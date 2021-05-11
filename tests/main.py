00904ef7acd7bdab52a931d78bda3353e73b900d
宮地宏樹 <hdekm852@gmail.com>
2021-05-11 20:51:59 +0900

    Version up

--- tests/main.py
+++ tests/main.py
@@ -13,9 +13,11 @@ def main():
     print(loader.load_class(".tests.clazz.base.module_1"))
     print(loader.load_classes("tests.clazz.packageA"))
     print(loader.load_classes("tests.clazz.packageA", recursive=True))
+    print(loader.load_classes("tests.clazz.base.packageD"))
     print(loader.load_function("./tests/func/base/func1.py"))
     print(loader.load_functions("tests/func/packageA"))
     print(loader.load_functions("tests/func/packageA", recursive=True))
+    print(loader.load_functions("tests/func/base/packageD"))
     print("--- end test -------------------------")
 
 
