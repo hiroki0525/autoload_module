b9c1763900c306ebf460c5c6cd1c455c67ccc78c
宮地宏樹 <hdekm852@gmail.com>
2021-05-15 09:14:03 +0900

    Fix test cases

--- tests/clazz/base/packageD/module_d1.py
+++ tests/clazz/base/packageD/module_d1.py
@@ -2,16 +2,16 @@ from autoload.decorator import load_config
 from tests.clazz.test_module import TestModule
 
 
-@load_config(load=True, order=2)
+@load_config(order=2)
 class ModuleD2(TestModule):
     pass
 
 
-@load_config(load=True, order=1)
+@load_config(order=1)
 class ModuleD1(TestModule):
     pass
 
 
-@load_config(load=True, order=3)
+@load_config(order=3)
 class ModuleD3(TestModule):
     pass
