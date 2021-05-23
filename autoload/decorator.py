0831672e5d730537f518329c598530738f2ccbd4
宮地宏樹 <hdekm852@gmail.com>
2021-05-23 22:32:20 +0900

    Add type hint to load_config

--- autoload/decorator.py
+++ autoload/decorator.py
@@ -1,7 +1,9 @@
 __all__ = "load_config"
 
+from typing import Optional
 
-def load_config(order=None, load=True):
+
+def load_config(order: Optional[int] = None, load: bool = True):
     def decorator(resource):
         if order:
             resource._load_order = order
