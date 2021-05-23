001d20b6b6f2848127659ebae96785339518cfee
宮地宏樹 <hdekm852@gmail.com>
2021-05-23 23:03:03 +0900

    Use setattr

--- autoload/decorator.py
+++ autoload/decorator.py
@@ -6,8 +6,8 @@ from typing import Optional
 def load_config(order: Optional[int] = None, load: bool = True):
     def decorator(resource):
         if order:
-            resource._load_order = order
-        resource._load_flg = load
+            setattr(resource, "_load_order", order)
+        setattr(resource, "_load_flg", load)
         return resource
 
     return decorator
